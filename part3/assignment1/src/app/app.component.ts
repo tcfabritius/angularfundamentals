4bb4c642046c25e962d7d652ce149a42d3c47cde
Tim Fabritius <tim.fabritius@skiff.com>
2024-12-29 11:06:51 +0200

    Asignment1

---
+++ part3/assignment1/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'assignment1';
+  rnd: number | undefined;
+  
+  randomNumber(){
+    this.rnd = Math.floor(Math.random()*100)
+  }
+}
